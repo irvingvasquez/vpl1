79d8b659bd287c67be2541916d5b2f983aadb874
Irving Vasquez <ivasquez@ccc.inaoep.mx>
2019-12-16 20:02:33 -0600

    add robot class

---
+++ include/vpl1/pmutils.h
@@ -0,0 +1,99 @@
+/*
+ * 
+ * 
+Partial Model Library
+Copyright (c) 2016, J. Irving Vasquez ivasquez@ccc.inaoep.mx
+Consejo Nacional de Ciencia y Tecnología (CONACYT)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+* Redistributions of source code must retain the above copyright notice, this
+  list of conditions and the following disclaimer.
+
+* Redistributions in binary form must reproduce the above copyright notice,
+  this list of conditions and the following disclaimer in the documentation
+  and/or other materials provided with the distribution.
+
+* Neither the name of the copyright holder nor the names of its
+  contributors may be used to endorse or promote products derived from
+  this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
+
+
+#include <vector>
+//#include <math.h>
+#include <octomap/Pointcloud.h>
+#include <octomap/octomap.h>
+#include <octomap/ColorOcTree.h>
+#include <octomap/OcTreeBaseImpl.h>
+#include <octomap/OccupancyOcTreeBase.h>
+
+#include <iostream>
+//#include "vpfilereader.h"
+#include "vptriangle.h"
+
+#include <boost/numeric/ublas/matrix.hpp>
+#include <boost/numeric/ublas/io.hpp> 
+
+//TODO change the name of this file and namespace
+
+namespace PMUtils {
+
+  double euclideanDistance (std::vector< int > A, std::vector< int > B);
+    
+  double euclideanDistance (std::vector<double> A, std::vector<double> B);
+
+  void printVector( std::vector<int> v);
+  
+  void printVector( std::vector<float> v);
+  
+  void printVector( std::vector<double> v);
+
+  bool utilsAreBoolVectorsEqual(std::vector<bool> A, std::vector<bool> B);
+
+  void utilsGetActivatedMotors(std::vector<int> u, std::vector<bool> &activations);
+
+  int applyIncrement(const std::vector<int> &q_t, const std::vector<int> &control, std::vector<int> &q_tmas1);
+
+  bool raw2triangles(const std::string file_raw, const std::string file_tri);
+  
+  bool saveCoordinatesAsVRML( std::vector< std::vector<double> > data, std::string file_name);
+  
+  /**
+   * Reads the files and return the percentage of matched points based on the reference file
+   */
+  double compareFilePoints(std::string file_target, std::string file_reference, double gap);
+  
+  double getCoverage(octomap::Pointcloud target, octomap::Pointcloud reference, double gap);
+    
+  /*
+  * gets the homogeneous transformation matrix from a pose (x,y,z, yaw(z), pitch(y), roll(x))
+  * status: not tested
+  */
+  void getHTMfromPose(std::vector<double> pose, boost::numeric::ublas::matrix<double> &HTM); 
+  
+  /*
+  * gets the homogeneous transformation matrix from a pose (x,y,z, yaw(z), pitch(y), roll(x))
+  * status: not tested
+  */
+  void getHTMfromPose(octomath::Pose6D pose, boost::numeric::ublas::matrix<double> &HTM); 
+  
+  /**
+   * Before calling this function stablish the tree BBx and expand the tree
+   */
+  void saveOctomapAsText(octomap::ColorOcTree* tree, std::string file_name);
+  
+}
---
+++ src/pmutils.cpp
@@ -0,0 +1,379 @@
+#include <vpl1/pmutils.h>
+
+
+bool PMUtils::raw2triangles(const std::string file_raw, const std::string file_tri)
+{
+  vpTriangleList tl;
+  
+  tl.readFile(file_raw);
+  tl.saveToMSLTriangle(file_tri);
+  
+  return true;
+}
+
+
+bool PMUtils::saveCoordinatesAsVRML(std::vector< std::vector<double> > data, std::string file_name)
+{
+  std::vector< std::vector<double> >::iterator it;
+  octomap::Pointcloud pc;
+  
+  for(it = data.begin(); it!= data.end(); it++){
+
+    octomap::point3d p((*it)[0],(*it)[1],(*it)[2]);    
+    pc.push_back(p);
+  }
+  
+  pc.writeVrml(file_name);
+  return true;
+}
+
+
+double PMUtils::euclideanDistance(std::vector< int > A, std::vector< int > B)
+{
+   std::vector<int>::iterator ita;
+   std::vector<int>::iterator itb;
+   
+   double sum = 0.0;
+   double distance =  0.0;
+   double resta = 0.0;
+  // int a=0;
+  // int b=0;
+   
+   itb = B.begin();
+   for(ita = A.begin(); ita != A.end(); ita++ ){
+     // a = *ita;
+      resta = (double) (*ita) - (double) (*itb);
+      sum = sum + pow(resta, 2);
+      itb ++;
+    }
+    
+    distance = sqrt(sum);
+    return distance;
+}
+
+
+double PMUtils::euclideanDistance(std::vector< double > A, std::vector< double > B)
+{
+   std::vector<double>::iterator ita;
+   std::vector<double>::iterator itb;
+   
+   double sum = 0.0;
+   double distance =  0.0;
+   double resta = 0.0;
+  // int a=0;
+  // int b=0;
+   
+   itb = B.begin();
+   for(ita = A.begin(); ita != A.end(); ita++ ){
+     // a = *ita;
+      resta = (double) (*ita) - (double) (*itb);
+      sum = sum + pow(resta, 2);
+      itb ++;
+    }
+    
+    distance = sqrt(sum);
+    return distance;
+}
+
+
+void PMUtils::printVector(std::vector< int > v)
+{
+  std::vector<int>::iterator it;
+  for(it = v.begin(); it!= v.end(); it++){
+    std::cout << " " << *it;
+  }
+  std::cout <<std::endl;
+}
+
+void PMUtils::printVector(std::vector< float > v)
+{
+  std::vector<float>::iterator it;
+  for(it = v.begin(); it!= v.end(); it++){
+    std::cout << " " << *it;
+  }
+  std::cout << std::endl;
+}
+
+void PMUtils::printVector(std::vector< double > v)
+{
+  std::vector<double>::iterator it;
+  for(it = v.begin(); it!= v.end(); it++){
+    std::cout << " " << *it;
+  }
+  std::cout << std::endl;
+}
+
+
+
+bool PMUtils::utilsAreBoolVectorsEqual(std::vector< bool > A, std::vector< bool > B)
+{
+  if(A.size() != B.size())
+    return false;
+  
+  std::vector<bool>::iterator it;
+  std::vector<bool>::iterator it_B;
+  it_B = B.begin();
+  for(it=A.begin(); it!=A.end(); it++){
+    
+    if(*it != *it_B)
+      return false;
+    
+    it_B++;
+  }
+  
+  return true;
+}
+
+
+void PMUtils::utilsGetActivatedMotors(std::vector< int > u, std::vector< bool >& activations)
+{
+  activations.clear();
+//  activations.resize(u.size());
+  std::vector<int>::iterator it;
+  bool value;
+  
+  for(it=u.begin(); it!=u.end(); it++){
+    if((*it) == 0){
+      value = false;
+      activations.push_back(value);
+    }
+    else{
+      value = true;
+      activations.push_back(value);
+    }
+  }
+  
+  //cout << activations.size() <<std::endl;
+}
+
+
+int PMUtils::applyIncrement(const std::vector< int >& q_t, const std::vector< int >& control, std::vector< int >& q_tmas1)
+{
+    q_tmas1.clear();
+    q_tmas1.resize(q_t.size());
+    
+    int aux;
+    
+    // apply control and determine the next configuration
+    for(int i = 0; i< q_t.size(); i++){
+        aux = q_t[i] + control[i];
+	q_tmas1[i] = aux;
+    }
+    
+    return 0;
+}
+
+
+double PMUtils::compareFilePoints(std::string file_target, std::string file_reference, double gap)
+{
+   //string file_a("/home/irving/projects/nbvPlanning-1.1/test/EVA/data/object_accumulated_points.dat");
+   std::string file_a(file_target);
+   
+   //string file_reference("/home/irving/Blensor/scenes/teapot_ref.dat");
+//   std::string file_reference(argv[2]);
+//   std::string output_file(argv[3]);
+   
+   std::cout << "file to read: " << file_a << std::endl; 
+   
+   vpFileReader reader;
+   
+   std::vector< std::vector<double> > a,reference;
+   reader.readDoubleCoordinates(file_a, a);
+   reader.readDoubleCoordinates(file_reference, reference);
+   
+   //double gap = 0.004;
+   long int correspondences=0;
+   
+   for(int i = 0 ; i < reference.size();i++){
+     for(int j= 0; j< a.size(); j++)
+     {
+       if( fabs(reference[i][0] - a[j][0]) < gap ){
+	  if( fabs(reference[i][1] - a[j][1]) < gap ){
+	   if( fabs(reference[i][2] - a[j][2]) < gap ){
+	     correspondences ++;
+	     break;
+	   }
+	  }
+       }
+     }
+   }
+   
+   std::cout << "Correspondences: " << correspondences << std::endl;
+   double percentage = (correspondences / (double) reference.size()) * 100 ;
+   std::cout << "%" << percentage << std::endl;
+   
+   return percentage;
+}
+
+
+double PMUtils::getCoverage(octomap::Pointcloud target, octomap::Pointcloud reference, double gap)
+{
+    //double gap = 0.004;
+   long int correspondences=0;
+   
+   octomap::Pointcloud::iterator it_reference;
+   octomap::Pointcloud::iterator it_target;
+   
+   for(it_reference = reference.begin(); it_reference != reference.end(); it_reference++){
+     for(it_target = target.begin(); it_target != target.end(); it_target ++)
+     {
+       if ( (fabs(it_reference->x()-it_target->x())<gap ) 
+           && ( fabs(it_reference->y()-it_target->y())<gap ) 
+           && ( fabs(it_reference->z()-it_target->z())<gap ) )
+       {
+            correspondences ++;
+            break;
+       }
+     }
+   }
+   
+   std::cout << "Correspondences: " << correspondences << std::endl;
+   double percentage = (correspondences / (double) reference.size()) * 100 ;
+   std::cout << "%" << percentage << std::endl;
+   
+   return percentage;
+}
+
+
+
+/** 
+ * pose = (x,y,z,yaw,pitch,roll)
+ * 
+ */
+void PMUtils::getHTMfromPose(std::vector< double > pose, boost::numeric::ublas::matrix< double > &HTM)
+{
+  if(pose.size()!= 6)
+  {
+    std::cout << "ERROR: wrong pose size, getHTMfromPose" << std::endl;
+    return;
+  }
+  
+  boost::numeric::ublas::matrix< double > result(4,4);
+  double x,y,z, yaw, pitch, roll;
+  x = pose[0];
+  y = pose[1];
+  z = pose[2];
+  yaw = pose[3];
+  pitch = pose[4];
+  roll = pose[5];
+  
+  double cy = cos(yaw);
+  double sy = sin(yaw);
+  double cp = cos(pitch);
+  double sp = sin(pitch);
+  double cr = cos(roll);
+  double sr = sin(roll);
+
+  result(0,0) = cy * cp;  
+  result(0,1) = cy * sp *sr - sy * cr;
+  result(0,2) = cy * sp * cr + sy * sr;
+  result(0,3) = x;
+  
+  result(1,0) = sy * cp;
+  result(1,1) = sy * sp * sr + cy * cr;
+  result(1,2) = sy * sp * cr - cy * sr;
+  result(1,3) = y;
+  
+  result(2,0) = -sp;
+  result(2,1) = cp*sr;
+  result(2,2) = cp *cr;
+  result(2,3) = z;
+  
+  result(3,0) = 0;
+  result(3,1) = 0;
+  result(3,2) = 0;
+  result(3,3) = 1;
+  
+  //std::cout << "result:" << result << "\n" << std::endl;
+  HTM = result;
+}
+
+void PMUtils::getHTMfromPose(octomath::Pose6D pose, boost::numeric::ublas::matrix< double >& HTM)
+{ 
+  boost::numeric::ublas::matrix< double > result(4,4);
+  double x,y,z, yaw, pitch, roll;
+  x = pose.x();
+  y = pose.y();
+  z = pose.z();
+  yaw = pose.yaw();
+  pitch = pose.pitch();
+  roll = pose.roll();
+  
+  double cy = cos(yaw);
+  double sy = sin(yaw);
+  double cp = cos(pitch);
+  double sp = sin(pitch);
+  double cr = cos(roll);
+  double sr = sin(roll);
+
+  result(0,0) = cy * cp;  
+  result(0,1) = cy * sp *sr - sy * cr;
+  result(0,2) = cy * sp * cr + sy * sr;
+  result(0,3) = x;
+  
+  result(1,0) = sy * cp;
+  result(1,1) = sy * sp * sr + cy * cr;
+  result(1,2) = sy * sp * cr - cy * sr;
+  result(1,3) = y;
+  
+  result(2,0) = -sp;
+  result(2,1) = cp*sr;
+  result(2,2) = cp *cr;
+  result(2,3) = z;
+  
+  result(3,0) = 0;
+  result(3,1) = 0;
+  result(3,2) = 0;
+  result(3,3) = 1;
+  
+  //std::cout << "result:" << result << "\n" << std::endl;
+  HTM = result;
+}
+
+
+void PMUtils::saveOctomapAsText(octomap::ColorOcTree* tree, std::string file_name)
+{
+    
+    //octomap::AbstractOcTree* tree = octomap::AbstractOcTree::read (octo_input);
+    double occu_voxels = 0, free_voxels = 0;
+    octomap::point3d p;
+    int m=0;
+    
+    //octomap::ColorOcTree ot = dynamic_cast<octomap::ColorOcTree>(tree);
+    
+    if (tree){
+        std::ofstream output_file(file_name, std::ios::app);
+        //cout << "\n octomap read... creating file" << endl;
+        //tree->setBBXMax(x_max);
+        //tree->setBBXMin(x_min);
+        //tree->expand();
+        //cout << "\n bouning box " << tree->getBBXBounds() << endl;
+        //cout << "\n resolution " << tree->getResolution() << endl;
+        for (octomap::ColorOcTree::leaf_iterator it = tree->begin_leafs(), end = tree->end_leafs(); it!=end; ++it){
+            p = it.getCoordinate();
+            if (tree->inBBX(p)){
+            //cout << "\n coord: " << p << endl;
+            if (tree->isNodeOccupied(*it)){
+                occu_voxels++;
+                output_file << p(0) <<" " << p(1) << " " << p(2) << " " << (it->getOccupancy()) << std::endl;
+            }
+            else{
+                free_voxels++;
+                output_file <<p(0) <<" " << p(1) << " " << p(2) << " " << (it->getOccupancy()) << std::endl;
+            }
+            }
+            m++;
+        }
+        output_file.close();
+   }
+   else
+     std::cout << "\n void tree" << std::endl;
+    
+   //delete tree;
+   //delete ot;
+   //delete p;
+   std::cout << "Octomap saved as text \n\t occupied voxels = " << occu_voxels << std::endl;
+   std::cout << "\n\t free voxels = " << free_voxels << std::endl;
+}
+
+
